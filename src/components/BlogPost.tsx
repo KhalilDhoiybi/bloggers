279c689574e201d95343963cc787f2988c45aaf9
KhalilDhoiybi <dhoiybikhalil@gmail.com>
2023-03-26 21:12:46 +0100

    delete blog btn ui

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,4 +1,6 @@
+import { TrashIcon } from "@heroicons/react/24/solid";
 import type { Blog, User } from "@prisma/client";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { type FC } from "react";
@@ -9,6 +11,8 @@ interface BlogProps {
 }
 
 const BlogPost: FC<BlogProps> = ({ blog, user }) => {
+  const { data: session } = useSession();
+
   return (
     <div className="rounded-lg border border-slate-500">
       <div className="flex items-center justify-between border-b border-slate-500 px-8 py-5">
@@ -24,9 +28,16 @@ const BlogPost: FC<BlogProps> = ({ blog, user }) => {
             @{user.name}
           </Link>
         </div>
-        <p className="px-8 py-2 text-slate-400">
-          Posted At: {blog.createdAt.toLocaleString()}
-        </p>
+        <div className="flex items-center gap-4">
+          <p className="py-2 text-slate-400">
+            Posted At: {blog.createdAt.toLocaleString()}
+          </p>
+          {session?.user.id === user.id && (
+            <button>
+              <TrashIcon className="h-5 w-5 text-slate-400 hover:text-slate-200" />
+            </button>
+          )}
+        </div>
       </div>
       <div className="space-y-6 px-8 py-6">
         <h1 className="text-center text-2xl font-bold">{blog.title}</h1>
